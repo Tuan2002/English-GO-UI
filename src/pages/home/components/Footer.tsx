1180fbf693d8a893ad663660191e85f0030653b7
Nguyen Ta Quyen <ta2k3quyen@gmail.com>
2025-02-19 20:14:12 +0700

    update copyright

--- src/pages/home/components/Footer.tsx
+++ src/pages/home/components/Footer.tsx
@@ -1,11 +1,12 @@
-import style from "../HomePage.module.scss";
 import classNames from "classnames/bind";
+import style from "../HomePage.module.scss";
 const cx = classNames.bind(style);
 const Footer = () => {
   return (
     <div className={cx("footer")}>
       <span className='text-center'>
-        Copyright © By <span className='font-weight-600'>Nguyễn Tạ Quyền</span> <br /> 62K3 CNTT - Version 1.0.1
+        Copyright © By <span className='font-weight-600'>Nguyễn Tạ Quyền</span> &{" "}
+        <span className='font-weight-600'>Nguyễn Ngọc Anh Tuấn</span> <br /> 62K3 CNTT - Version 1.0.2
       </span>
     </div>
   );
